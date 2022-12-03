5a2cac004a33d9a9b0f42259a862d8d26930218b
joru33 <joru33@web.de>
2022-12-03 22:39:02 +0100

    AttackBar v2
    Still not done yet

--- Singularity-Game/Assets/Scripts/MeleeWeapon.cs
+++ Singularity-Game/Assets/Scripts/MeleeWeapon.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class MeleeWeapon : MonoBehaviour
 {
+    public int currentAttack = 0;
+    public int maxAttack = 4;
+    private float attackTimer = 0;
 
+    public AttackStrength attack; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        attack.AttackInit(maxAttack);
+        StartCoroutine(waiter());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        LoadAttack(); 
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,5 +29,31 @@ public class MeleeWeapon : MonoBehaviour
         {
             other.GetComponent<MeshRenderer>().enabled = false;
         }
+    }
+
+    private void LoadAttack()
+    {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            attackTimer += Time.deltaTime;
+            currentAttack = (int) Mathf.Floor(attackTimer);
+            attack.SetAttack(currentAttack);
+            waiter();
+        }
+    }
+
+    private void RealeaseAttack()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            attackTimer = 0;
+            currentAttack
+        }
+    }
+
+    IEnumerator waiter()
+    {
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(3);
     }
 }
