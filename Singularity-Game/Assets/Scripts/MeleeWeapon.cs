b473e8f113526be11bb8cc70eac60eadce9dddad
joru33 <joru33@web.de>
2022-12-03 23:36:00 +0100

    AttackBar v3
    Concept implemented, works alright, improvements will be made

--- Singularity-Game/Assets/Scripts/MeleeWeapon.cs
+++ Singularity-Game/Assets/Scripts/MeleeWeapon.cs
@@ -7,13 +7,16 @@ public class MeleeWeapon : MonoBehaviour
     [SerializeField ]GameObject player;
     [SerializeField] int damagePerHit = 40;
     public int currentAttack = 0;
-    public int maxAttack = 4;
+    public int maxAttack = 100;
     private float attackTimer = 0;
+    private GameObject activeBar;
 
     public AttackStrength attack; 
     // Start is called before the first frame update
     void Start()
     {
+        activeBar = GameObject.Find("AttackStrengthBar");
+        activeBar.SetActive(false);
         attack.AttackInit(maxAttack);
         StartCoroutine(waiter());
     }
@@ -21,7 +24,8 @@ public class MeleeWeapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        LoadAttack(); 
+        LoadAttack();
+        ReleaseAttack();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -40,21 +44,50 @@ public class MeleeWeapon : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Mouse1))
         {
+            activeBar.SetActive(true);
             attackTimer += Time.deltaTime;
             currentAttack = (int) Mathf.Floor(attackTimer);
             attack.SetAttack(currentAttack);
-            waiter();
+            //waiter();
         }
     }
 
-    private void RealeaseAttack()
+    private void ReleaseAttack()
     {
         if (Input.GetKeyUp(KeyCode.Mouse1))
         {
+            EnemyPull();
             attackTimer = 0;
-            currentAttack
+            currentAttack = 0;
+            attack.SetAttack(currentAttack);
+            activeBar.SetActive(false);
         }
     }
+
+    void EnemyPull()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position + Vector3.up, transform.TransformDirection(Vector3.forward));
+        LayerMask hitLayer = LayerMask.NameToLayer("Enemy");
+
+        Debug.DrawRay(transform.position + Vector3.up, transform.TransformDirection(Vector3.forward), Color.red, 5);
+
+        float sphereRadius = 2;
+        float maxDistance = 20;
+        int layerMask = (1 << hitLayer);
+        if (Physics.SphereCast(ray, sphereRadius, out hit, maxDistance, layerMask))
+        {
+            Debug.Log("There is an enemy!");
+            Rigidbody enemy = hit.rigidbody;
+            enemy.AddForce(transform.TransformDirection(Vector3.back) * 50 * currentAttack);
+        }
+        else
+        {
+            Debug.Log("There is no enemy!");
+        }
+
+    }
+
 
     IEnumerator waiter()
     {
