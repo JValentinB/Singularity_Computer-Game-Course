741b5997ad502cf5fd5be6c4fdb964108fde4191
Kruestchen <kevin.plessing@student.uni-tuebingen.de>
2022-12-03 15:39:33 +0100

    NewGravitySceneAndCleanedCode

--- Singularity-Game/Assets/Scripts/PlayerControl.cs
+++ Singularity-Game/Assets/Scripts/PlayerControl.cs
@@ -1,23 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField] private float walking_speed = 0.4f;
-    [SerializeField] private float running_speed = 3.0f;
-    [SerializeField] private float sprinting_speed = 4.0f;
-    [SerializeField] private float jumpforce = 400;
-    [SerializeField] private int airjumps = 2;
-    private int jumpnumber;
-    private float speed;
+    //declare and initialize constants
+    [SerializeField] private static int     FIELDS          = 1;
+    [SerializeField] private static float   FIELDFACTOR     = 2.0f;
+    [SerializeField] private static float   START_MASS      = 75.0f;
+    [SerializeField] private float          WALK            = 0.4f;
+    [SerializeField] private float          RUN             = 3.0f;
+    [SerializeField] private float          SPRINT          = 4.0f;
+    [SerializeField] private int            JUMPFACTOR      = 200;
+    
 
-    private float direction = -1;
-    private Animator animator;
-    private Rigidbody rigidbody;
-    private GameObject weapon;
 
-    private float lastPosY;
+    //declare variables
+    private float           walking_speed;
+    private float           running_speed;
+    private float           sprinting_speed;
+    private float           jumpforce;
+    private float           speed;
+    private float           direction;
+    private float           lastPosY;
+    private int             jumpnumber;
+    private bool            jumpboots;
+    private Animator        animator;
+    private Rigidbody       rigidbody;
+    private GameObject      weapon;
+
+
+    [SerializeField] private int             airjumps;
+    [SerializeField] private GameObject[]    gravityFields = new GameObject[FIELDS];
+
+
+
+
+    //=========================================================================================================================================
+
+    //=========================================================================================================================================
 
     // Start is called before the first frame update
     void Start()
@@ -26,32 +49,80 @@ public class PlayerControl : MonoBehaviour
         rigidbody = GetComponent<Rigidbody>();
         lastPosY = this.transform.position.y;
 
-        jumpnumber = airjumps;
+        airjumps = 0;
+        rigidbody.mass = START_MASS;
+        jumpforce = START_MASS * JUMPFACTOR;
+        jumpnumber = jumpnum();
+        direction = -1;
+        jumpboots = false;
+
     }
-    
+
+    //=========================================================================================================================================
+
+    //=========================================================================================================================================
+
     // Update is called once per frame
     void Update()
     {
-        // press shift to run fast
+        speedUpdate();
+
+        walk_run_sprint();
+
+        directionChange();
+
+        foo();
+
+        jump();
+
+        changeEquipment();
+
+        GroundCheck();
+        
+        updateMass(gravityFields);
+    }
+
+    //=========================================================================================================================================
+                                                                                                                                         
+    //=========================================================================================================================================
+
+    void speedUpdate()
+    {
+        float ratio_mass_speed = START_MASS / rigidbody.mass;
+        walking_speed = WALK * ratio_mass_speed;
+        running_speed = RUN * ratio_mass_speed;
+        sprinting_speed = SPRINT * ratio_mass_speed;
+    }
+
+    void walk_run_sprint()
+    {
+        // press shift to run fast, or strg/cmd to walk
         if (Input.GetKey(KeyCode.LeftControl)) speed = walking_speed;
         else if (Input.GetKey(KeyCode.LeftShift)) speed = sprinting_speed;
         else speed = running_speed;
+    }
 
+    void directionChange()
+    {
         // turn around
         if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) direction = 1;
         if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)) direction = -1;
         this.transform.rotation = Quaternion.Euler(0, direction * 90, 0);
+    }
 
+    void foo()
+    {
         // Running
         float landing = (animator.GetCurrentAnimatorStateInfo(0).IsName("Landing")) ? 0.5f : 1;
         var velocity = direction * Vector3.forward * Input.GetAxis("Horizontal") * landing * speed;
         transform.Translate(velocity * Time.deltaTime);
-        //rigidbody.AddForce(new Vector3(velocity.x,0,velocity.z) * rigidbody.mass * 100);
         animator.SetFloat("Speed", velocity.magnitude);
+    }
 
-
+    void jump()
+    {
         // Jumping and how many airjumps you can do
-        if (animator.GetBool("Jumping")) 
+        if (animator.GetBool("Jumping"))
             animator.SetBool("Jumping", false);
         if (Input.GetKeyDown(KeyCode.Space) && jumpnumber > 0)
         {
@@ -60,14 +131,18 @@ public class PlayerControl : MonoBehaviour
             rigidbody.AddForce(Vector3.up * jumpforce);
             jumpnumber--;
         }
+    }
 
+    void changeEquipment()
+    {
         // Scroll Mouse Wheel to change Equipment
         if (Input.mouseScrollDelta.y != 0)
         {
             animator.SetInteger("Equipment", (animator.GetInteger("Equipment") + 1) % 2);
         }
         // Show equipped Weapon
-        if (animator.GetInteger("Equipment") == 0) {
+        if (animator.GetInteger("Equipment") == 0)
+        {
             weapon = GameObject.Find("Gun");
             weapon.GetComponent<MeshRenderer>().enabled = false;
         }
@@ -77,10 +152,32 @@ public class PlayerControl : MonoBehaviour
             weapon.GetComponent<MeshRenderer>().enabled = true;
         }
         this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 0);
-
-        GroundCheck();
     }
-    
+
+    // checks if player is in a GravityField and changes mass, if necessary
+    void updateMass(GameObject[] gravityFields)
+    {
+        float player_x = this.transform.position.x;
+        float player_y = this.transform.position.y;
+
+        for (int i = 0; i < FIELDS; i++)
+        {
+            
+            if (gravityFields[i].transform.position.x - gravityFields[i].transform.localScale.x * 0.5f < player_x &&
+                gravityFields[i].transform.position.x + gravityFields[i].transform.localScale.x * 0.5f > player_x &&
+                gravityFields[i].transform.position.y - gravityFields[i].transform.localScale.y * 0.5f < player_y &&
+                gravityFields[i].transform.position.y + gravityFields[i].transform.localScale.y * 0.5f > player_y)
+            {
+  
+                rigidbody.mass = 75.0f * FIELDFACTOR;
+                Console.WriteLine(rigidbody.mass);
+                return;
+            }
+        }
+        rigidbody.mass = START_MASS;
+        return;
+    }
+
     // Checks how far away the ground is and sets the Falling bool
     void GroundCheck()
     {
@@ -97,11 +194,25 @@ public class PlayerControl : MonoBehaviour
             else {
                 animator.SetBool("Falling", false);
                 // reset airjump number
-                jumpnumber = airjumps;
+                jumpnumber = jumpnum();
             }
 
 
 
         }
+    }
+
+    int jumpnum()
+    {
+        return airjumpnum() + 1;
+    }
+
+    int airjumpnum()
+    {
+        if (jumpboots)
+        {
+            return 1;
+        }
+        else return 0;
     }
 }
---
+++ Singularity-Game/README.md
@@ -0,0 +1,13 @@
+# Singularity_Computer-Game-Course
+
+We are developing our own computer game for the Computer Games & Visual Effects-Course!
+There is only one specification given, the keyword for the primary game mechanic is "Attraction".
+
+Our game will be a metroidvania, a 2D-Jump'n'Run/Sidescroller. As the player you will discover an
+interconnected map of several areas. To reach the next area you first have to find special items
+or skills. 
+
+All items, skills and weapons are centered around the keyword "Attraction". Furthermore we interpret
+"Attraction" as a physical term and will be used as a synonym for gravity. 
+  
+
