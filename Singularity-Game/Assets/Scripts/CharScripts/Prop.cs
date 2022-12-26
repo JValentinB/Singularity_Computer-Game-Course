5bdde249f338bb56ce984207156a792083130faa
Clichee <timur_aydin.33@hotmail.de>
2022-12-26 19:48:14 +0100

    small updatge on prop

--- Singularity-Game/Assets/Scripts/CharScripts/Prop.cs
+++ Singularity-Game/Assets/Scripts/CharScripts/Prop.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class Prop : Damageable
 {
+    void Start(){
+        maxHealth = 1;
+        currentHealth = maxHealth;
+        gravitationalDirection = Vector3.down;
+        direction = 1;
+        rigidbody = GetComponent<Rigidbody>();
+    }
 
+    void FixedUpdate(){
+        ApplyGravity();
+        RotateGravity();
+    }
+
+    public void onDeath(){
+        //destroy prop
+        //spawn all items from inventory
+    }
 }
