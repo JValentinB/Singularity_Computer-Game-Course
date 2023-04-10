bc40d3777208b2c4757b64ee1d0b76fd6d49d448
joru33 <joru33@web.de>
2023-04-10 15:35:46 +0200

    Update countScript.js

--- countScript.js
+++ countScript.js
@@ -1,11 +1,10 @@
 function downloadCounter(){
     const anchor = document.querySelector("#DLB");
     anchor.addEventListener("click",function(){
-        fetch("./downloaded.txt")
+        fetch("downloaded.txt")
         .then(response => response.text()) 
-        .then(textString => {
-            console.log(textString);
+        .then(textString => parseInt(textString, 10))
+        .then(num => writeFile("downloaded.txt",num+1))
         });
-    })
 }
 downloadCounter();
