b8e31ff0ee741ab0cacc8db5816f128dfcbe597b
joru33 <joru33@web.de>
2023-04-10 13:38:21 +0200

    try fix counter

--- countScript.js
+++ countScript.js
@@ -1,7 +1,7 @@
 function downloadCounter(){
     var anchor = document.querySelector("#DLB");
     anchor.addEventListener("click",function(){
-        fetch("downloadResponse.php");
+        fetch("./downloadResponse.php");
     })
 }
 downloadCounter();
--- downloadResponse.php
+++ downloadResponse.php
@@ -1,10 +1,10 @@
 <?php
 
 if (!is_file("downloaded.txt")) {
-    file_put_contents("downloaded.txt", 0);
+    file_put_contents("./downloaded.txt", 0);
 }
 
-$dl = file_get_contents("downloaded.txt");
-file_put_contents("downloaded.txt",++$dl);
+$dl = file_get_contents("./downloaded.txt");
+file_put_contents("./downloaded.txt",++$dl);
 
 ?>
