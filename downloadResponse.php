b72910f5c5e0c9b09f1cb2bec2d06333b22473d2
joru33 <joru33@web.de>
2023-04-10 15:59:55 +0200

    Update downloadResponse.php

--- downloadResponse.php
+++ downloadResponse.php
@@ -1,10 +1,15 @@
 <?php
 
-if (!is_file("./downloaded.txt")) {
+$file = fopen("downloaded.txt", "w");
+$dl = file_get_contents($file);
+fwrite($file, $dl+1);
+$fclose($file);
+
+/* if (!is_file("./downloaded.txt")) {
     file_put_contents("./downloaded.txt", 0);
 }
 
 $dl = file_get_contents("./downloaded.txt");
-file_put_contents("./downloaded.txt",$dl+1);
+file_put_contents("./downloaded.txt",$dl+1); */
 
 ?>
