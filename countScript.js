494223de8ad801406c494369e1e47424ff9d8731
joru33 <joru33@web.de>
2023-04-11 02:22:13 +0200

    Cleanup

--- countScript.js
+++
@@ -1,6 +0,0 @@
-function downloadCounter(){
-    const anchor = document.querySelector("#DLB");
-    anchor.addEventListener("click",function(){
-        fetch("downloadResponse.php")});
-}
-downloadCounter();
